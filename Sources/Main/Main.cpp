bfa569c4e31610d3b5f965e780d9c9972ecc6c0b
HotBlueStreak <HotBlueStreak@mail.ru>
2018-02-24 19:31:53 +0300

    Add SFML library

--- Sources/Main/Main.cpp
+++ Sources/Main/Main.cpp
@@ -1,4 +1,22 @@
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Game");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
